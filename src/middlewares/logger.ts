bbcc1fd6dec93954e71c31915cccc2df95e363db
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2021-05-20 16:51:54 +0530

    Logger time added

--- src/middlewares/logger.ts
+++ src/middlewares/logger.ts
@@ -6,7 +6,7 @@ const logger = (req: Request, res: Response, next: NextFunction) => {
   console.log(
     `${chalk.white.bold.inverse(` ${req.method} `)} ${req.protocol}://${req.get(
       "host"
-    )}${req.originalUrl} : ${format(new Date(), "dd-MMM-yyyy: mm- hh")}`
+    )}${req.originalUrl} : ${format(new Date(), "dd-MMM-yyyy: p")}`
   );
   next();
 };
