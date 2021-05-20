321e405ceb89d70c31704767bc46fcb329c1a826
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2021-05-20 18:03:01 +0530

    Index.js Lint Issue Fixed

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express, { Application, Request, Response, Router } from "express"
+import express, { Application, Request, Response } from "express"
 
 import logger from "./middlewares/logger"
 require("dotenv").config()
