66835a61849b2981b61241627bc448dae32edeab
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2021-05-20 16:35:33 +0530

    Basic Configuration

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+require("dotenv").config();
+import express, { Application, Request, Response } from "express";
+
+import logger from "./middlewares/logger";
+
+const app: Application = express();
+
+app.use(logger);
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello Typescript!");
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server Started at ${process.env.PORT}`);
+});
---
+++ src/middlewares/logger.ts
@@ -0,0 +1,14 @@
+import { format } from "date-fns";
+import chalk from "chalk";
+import { Request, Response, NextFunction } from "express";
+
+const logger = (req: Request, res: Response, next: NextFunction) => {
+  console.log(
+    `${chalk.white.bold.inverse(` ${req.method} `)} ${req.protocol}://${req.get(
+      "host"
+    )}${req.originalUrl} : ${format(new Date(), "dd-MMM-yyyy: mm- hh")}`
+  );
+  next();
+};
+
+export default logger;
