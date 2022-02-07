caf9e5a4bdbe12f319bd76e741e7802b34de733c
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2022-02-07 21:48:48 +0530

    Error Handler: Execute only if there's an error

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -1,10 +1,12 @@
 import { ErrorRequestHandler } from "express"
 
-const errorHandler: ErrorRequestHandler = (err, req, res) => {
+const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
+  if (!err) {
+    next()
+  }
   const statusCode = res.statusCode || 500
 
-  res.status(statusCode)
-  res.json({
+  res.status(statusCode).json({
     message: err.message,
     stack: process.env.NODE_ENV === "production" ? null : err.stack,
   })
