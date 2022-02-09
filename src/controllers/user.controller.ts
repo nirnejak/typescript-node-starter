6728e613b94f2f1e95d2a80fa8452d05c063de18
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2022-02-09 15:59:01 +0530

    Controller Snippet Added

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -3,11 +3,11 @@ import { Request, Response } from "express"
 import IUser from "../interfaces/user.interface"
 
 /**
- * @description Get Users
  * @route GET /api/users
  * @access Public
  * @param req Request Object
  * @param res Response Object
+ * @description Get Users
  */
 const getUsers = (req: Request, res: Response) => {
   const users: IUser[] = [
