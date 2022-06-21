87e3292f80a625fa2bbaf3093276e278e43347bc
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2022-06-21 23:42:59 +0530

    TODO comment added, type defs updated

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -6,6 +6,8 @@ const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
   }
   const statusCode = res.statusCode || 500
 
+  // TODO: add proper logging and error tracking here
+
   res.status(statusCode).json({
     message: err.message,
     stack: process.env.NODE_ENV === "production" ? null : err.stack,
