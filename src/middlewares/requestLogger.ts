3f8b53d5aa045b37d2b7cdb66ae89daec03ff665
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2022-07-11 15:46:50 +0530

    colorette added

--- src/middlewares/requestLogger.ts
+++ src/middlewares/requestLogger.ts
@@ -1,10 +1,11 @@
 import { Request, Response, NextFunction } from "express"
+import { white, bold } from "colorette"
 
 import utils from "../utils"
 
 const requestLogger = (req: Request, res: Response, next: NextFunction) => {
   console.log(
-    `${req.method} ${req.protocol}://${req.get("host")}${
+    `${bold(white(req.method))} ${req.protocol}://${req.get("host")}${
       req.originalUrl
     } : ${utils.formatToDateTime(new Date())}`
   )
