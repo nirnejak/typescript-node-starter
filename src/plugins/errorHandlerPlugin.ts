ec4c0848fd2977970bb05c3dea05528340bddcd9
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2024-11-23 21:21:41 +0530

    errorHandlerPlugin: eslint issue fixed

--- src/plugins/errorHandlerPlugin.ts
+++ src/plugins/errorHandlerPlugin.ts
@@ -1,10 +1,10 @@
-import { FastifyInstance } from "fastify"
+import { type FastifyInstance } from "fastify"
 
-async function errorHandlerPlugin(fastify: FastifyInstance) {
+async function errorHandlerPlugin(fastify: FastifyInstance): Promise<void> {
   fastify.setErrorHandler((error, request, reply) => {
     request.log.error(error)
 
-    if (error.validation) {
+    if (error.validation !== null) {
       reply.status(400).send({
         error: "Validation Error",
         message: error.message,
