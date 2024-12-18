47fce973b4619f9fd1d2f5df73c450e888728451
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2024-12-18 17:53:21 +0530

    errorHandlerPlugin: ts issues fixed

--- src/plugins/errorHandlerPlugin.ts
+++ src/plugins/errorHandlerPlugin.ts
@@ -1,10 +1,10 @@
-import { type FastifyInstance } from "fastify"
+import type { FastifyInstance } from "fastify"
 
-async function errorHandlerPlugin(fastify: FastifyInstance): Promise<void> {
+function errorHandlerPlugin(fastify: FastifyInstance): void {
   fastify.setErrorHandler((error, request, reply) => {
     request.log.error(error)
 
-    if (error.validation !== null) {
+    if (error.validation == null) {
       reply.status(400).send({
         error: "Validation Error",
         message: error.message,
