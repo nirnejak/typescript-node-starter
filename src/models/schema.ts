dc284f7a3ba61a9def836ecef9d6f58ace402182
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2025-02-18 17:10:52 +0530

    drizzle schema added

---
+++ src/models/schema.ts
@@ -0,0 +1,8 @@
+import { pgTable, serial, varchar, text } from "drizzle-orm/pg-core"
+
+export const users = pgTable("users", {
+  id: serial("id").primaryKey(),
+  name: varchar("name", { length: 255 }).notNull(),
+  email: varchar("email", { length: 255 }).notNull(),
+  password: text("password").notNull(),
+})
