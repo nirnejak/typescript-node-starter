23267d2d837ec82fed0d8116815f156f240a1eef
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2026-01-22 03:07:51 +0530

    auth util updated

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,8 +1,7 @@
 import { betterAuth } from "better-auth"
-
 import { drizzleAdapter } from "better-auth/adapters/drizzle"
 
-import { db } from "@/drizzle.config"
+import { db } from "./db"
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
