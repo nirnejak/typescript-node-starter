6d502c50eac50c3d60aa03278ff923b1b096d7e2
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2026-01-22 03:06:41 +0530

    drizzle config fixes

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,5 @@
 "use strict"
-import { neon } from "@neondatabase/serverless"
-import { drizzle } from "drizzle-orm/neon-http"
 import { defineConfig } from "drizzle-kit"
-
-const sql = neon(process.env.DATABASE_URL!)
-export const db = drizzle({ client: sql })
 
 export default defineConfig({
   schema: "**/schema.ts",
