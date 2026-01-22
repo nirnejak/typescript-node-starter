6e555b4c18827144363dd2573d4fbe660b5b7c2b
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2026-01-23 02:54:29 +0530

    Bun.env → process.env

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -12,8 +12,8 @@ export const auth = betterAuth({
   },
   socialProviders: {
     google: {
-      clientId: Bun.env.AUTH_GOOGLE_ID!,
-      clientSecret: Bun.env.AUTH_GOOGLE_SECRET!,
+      clientId: process.env.AUTH_GOOGLE_ID!,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET!,
     },
   },
 })
